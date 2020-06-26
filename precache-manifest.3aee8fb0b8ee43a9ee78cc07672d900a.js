bb6e428f00de88d510004eb9a9bdd1cd287ea6ba
pavankumarvanam <v.pavan@ailabs.in>
2020-06-26 14:45:51 +0530

    Updates

---
+++ precache-manifest.3aee8fb0b8ee43a9ee78cc07672d900a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0962d6736aef84076979c160ae2cc883",
+    "url": "/material-ui-react/index.html"
+  },
+  {
+    "revision": "73e14f6bfca1ad7b8544",
+    "url": "/material-ui-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e509dbec70f9204bb275",
+    "url": "/material-ui-react/static/js/2.df95ee78.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/material-ui-react/static/js/2.df95ee78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73e14f6bfca1ad7b8544",
+    "url": "/material-ui-react/static/js/main.92bb4fb8.chunk.js"
+  },
+  {
+    "revision": "3167cbd1f3c83ff6775f",
+    "url": "/material-ui-react/static/js/runtime-main.95e5a321.js"
+  }
+]);
